810fd954aed11186a9e0baed7ebeb0cbadec9038
Koistinen Miikka <koistim5@niksula.hut.fi>
2016-02-11 02:54:31 +0200

    music now works in the binary

--- setup.py
+++ setup.py
@@ -13,10 +13,8 @@ if sys.platform == "win32":
 
 packages = ["pygame", "os", "time", "random", "math", "time", "sys", "string"]
 #includes = ["gameinfo"]
-# include_files = [("Elevator_music.ogg", "Elevator_music.ogg"), ("Pat_and_Mat_intro.ogg", "Pat_and_Mat_intro.ogg"), 
-# 	("RedCheck.png", "RedCheck.png"), ("MazeIcon1.png", "MazeIcon1.png"), ("freesansbold.ttf", "freesansbold.ttf")]
+include_files = ["Elevator_music.ogg", "Pat_and_Mat_intro.ogg", "RedCheck.png","MazeIcon1.png"]
 # includes = []
-include_files = []
 
 excludes = ["Savegames", "highscores.ma", ".DS_Store", "testIO", "testmatrixalgorithm", ".pyc"]
 
